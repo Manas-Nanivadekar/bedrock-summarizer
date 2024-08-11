d6483522083eca1df186254969c949ef2f599b40
manas <manasnanivadekar@gmail.com>
2024-08-11 18:35:34 +0530

    working state

--- app.py
+++ app.py
@@ -2,7 +2,115 @@ import json
 import os
 import sys
 import boto3
+import streamlit as st
 
 # Imports for the Bedrock model
 from langchain.embeddings import BedrockEmbeddings
 from langchain.llms.bedrock import Bedrock
+
+
+# Data Ingestion
+
+import numpy as np
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFDirectoryLoader
+
+# Vectorization imports
+from langchain.vectorstores import FAISS
+
+# Model
+from langchain.prompts import PromptTemplate
+from langchain.chains.retrieval_qa.base import RetrievalQA
+
+# Bedrock Client
+bedrock = boto3.client(service_name="bedrock-runtime")
+bedrock_embeddings = BedrockEmbeddings(
+    model_id="amazon.titan-embed-text-v1", client=bedrock
+)
+
+
+# Data Ingestion
+def data_ingestion():
+    loader = PyPDFDirectoryLoader("data")
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=1000)
+
+    docs = text_splitter.split(documents)
+    return docs
+
+
+# Vectorization
+def vectorization(docs):
+    vector_store_faiss = FAISS.from_documents(docs, bedrock_embeddings)
+    vector_store_faiss.save("faiss_index")
+
+
+# Model
+def get_claude_llm():
+    llm = Bedrock(
+        model_id="ai21.j2-mid-v1", client=bedrock, modelkwargs={"max_tokens": 512}
+    )
+    return llm
+
+
+prompt_template = """
+
+Human: Use the following pieces of context to provide a 
+concise answer to the question at the end but usse atleast summarize with 
+250 words with detailed explaantions. If you don't know the answer, 
+just say that you don't know, don't try to make up an answer.
+<context>
+{context}
+</context
+
+Question: {question}
+
+Assistant:"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+
+def get_response_llm(llm, vectorstore_faiss, query):
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore_faiss.as_retriever(
+            search_type="similarity", search_kwargs={"k": 3}
+        ),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": PROMPT},
+    )
+    answer = qa({"query": query})
+    return answer["result"]
+
+
+def main():
+    st.set_page_config("Chat PDF")
+
+    st.header("Chat with PDF using AWS Bedrock💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    with st.sidebar:
+        st.title("Update Or Create Vector Store:")
+
+        if st.button("Vectors Update"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                vectorization(docs)
+                st.success("Done")
+
+    if st.button("Claude Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm = get_claude_llm()
+
+            # faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm, faiss_index, user_question))
+            st.success("Done")
+
+
+if __name__ == "__main__":
+    main()
